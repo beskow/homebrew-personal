051a3cb9424dfdc54a6e0a559655060dd9ac542f
Bjorn Beskow <bb@callistaenterprise.se>
2019-09-13 14:35:55 +0200

    Added elCapitan versions of iterm2 and jumpcut

---
+++ Casks/iterm2-elCapitan.rb
@@ -0,0 +1,24 @@
+cask 'iterm2-elCapitan' do
+  # note: "2" is not a version number, but an intrinsic part of the product name
+  version '3.1.7'
+  sha256 'd5496b3c42fe2eaf65befef7d6d6682cde3e8cd1f042f63343f821e8582b1ede'
+
+  url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
+  appcast 'https://iterm2.com/appcasts/final.xml'
+  name 'iTerm2'
+  homepage 'https://www.iterm2.com/'
+
+  auto_updates true
+  depends_on macos: '>= :yosemite'
+
+  app 'iTerm.app'
+
+  zap trash: [
+               '~/Library/Application Support/iTerm',
+               '~/Library/Application Support/iTerm2',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl*',
+               '~/Library/Caches/com.googlecode.iterm2',
+               '~/Library/Preferences/com.googlecode.iterm2.plist',
+               '~/Library/Saved Application State/com.googlecode.iterm2.savedState',
+             ]
+end
